6d4832a952be4519c6bc4a1937649141f56aa1f7
Tom Doder <tdoder89@gmail.com>
2022-10-24 14:13:42 -0500

    show updates to pizza question

--- ifStatements.js
+++ ifStatements.js
@@ -65,17 +65,12 @@ if (numQuarters >= 2) {
 // Experiment with manipulating the value held within both variables
 // to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 2;
 var hasSauce = true;
 
-if (cupsOfFlour === 1 && hasSauce === true) {
-  console.log("I cannot make pizza")
-} else if (cupsOfFlour === 5 && hasSauce === false) {
-  console.log("I cannot make pizza")
-} else if (cupsOfFlour === 2 && hasSauce === true) {
-  console.log("I can make pizza")
-} else if (cupsOfFlour === 3 && hasSauce === true) {
-  console.log("I can make pizza")
-} else   {
-  console.log("no pizza today")
+if (cupsOfFlour > 1 && hasSauce === true) {
+  console.log("I can make pizza");
+} else {
+  console.log("I cannot make a pizza");
 }
+
